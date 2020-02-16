6abe2be56923af2b548e7d3340a4f3c6f9a19a74
Davin Jimenez <31362013+Djjimenez895@users.noreply.github.com>
2020-02-15 19:31:50 -0800

    Created statistics object for keeping track of file extension frequences

---
+++ Statistics.py
@@ -0,0 +1,17 @@
+class Statistics(object):
+    """This class defines an object that keeps track
+       of the frequency of different file types in the directories
+       traversed. 
+    """
+    def __init__(self):
+        print("Creating Statistics object")
+        self.file_frequency_dict = {} # Dictionary for storing file extensions and their number of occurences
+        self.total_files = 0 # total number of files in the directories traversed
+
+    """ This method is for printing all the file types and how many times they occur
+    """
+    def print_file_frequences():
+        for key,value in self.file_frequency_dict.items():
+            print(key + " => " + value)
+
+
