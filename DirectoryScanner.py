0b8a3c4c2ed28929928f1bcbf398470cc91b8b2f
Davin Jimenez <31362013+Djjimenez895@users.noreply.github.com>
2020-02-23 21:19:42 -0800

    Add loop to keep asking for user input and validate input
    Previously, the program did not ask for user input more than once;
    the program now has a while loop that checks what the user entered.
    Additional checks are needed in the future to ensure valid input is
    given to the program.

--- DirectoryScanner.py
+++ DirectoryScanner.py
@@ -11,8 +11,7 @@ import numpy as np
 '''
 def graph_files(stats):
     # TODO: Add a label to each individual bar in the graph
-    # TODO: Document the methods
-    # TODO: Add feature that will keep asking for a directory if the one entered isn't a directory. 
+    # TODO: Add feature that will keep asking for a directory if the one entered isn't an actual directory. 
     plt.style.use('seaborn')
     x_axis_values = list(stats.file_frequency_dict.keys()) # File extensions
     y_axis_values = list(stats.file_frequency_dict.values()) # Number of occurences for a file extension
@@ -65,11 +64,24 @@ def traverse_directories(dir):
 
     Input: N/A
     Return: N/A
+    TODO: Add input validation for directories entered into the application
 '''
 def main():
+    directory = ""
     print("Please enter a directory:")
-    directory = input()
-    print(directory)
-    traverse_directories(directory)
+    finished_graphing = False
+
+    while(not finished_graphing):
+        directory = input()
+
+        if(directory == ""):
+            print("Please enter a directory:")
+        elif (directory == "exit" or directory == "e"):
+            finished_graphing = True
+        else:
+            print(directory)
+            traverse_directories(directory)
+
+    print("Exiting now")
 
 main()
